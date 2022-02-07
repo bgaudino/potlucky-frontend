21fe41c0318790073bb7d20bb00b944b95521459
brianpaperless <brian@thepaperlessagent.com>
2022-02-07 11:47:03 -0600

    Add copy to clipboard

--- src/components/PotluckInfo.tsx
+++ src/components/PotluckInfo.tsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
+import { Button, Snackbar } from "@mui/material";
 import { Potluck } from "../types";
 import { formatTime, formatDate } from "../utils/datetime";
 import Label from "./Label";
 
 export default function PotluckInfo(props: { potluck: Potluck }) {
   const { potluck } = props;
+  const [showSnackbar, setShowSnackbar] = useState(false);
+
+  function copyLink() {
+    const link = `${window.location.origin}/potlucks/${potluck._id}`;
+    navigator.clipboard.writeText(link);
+    setShowSnackbar(true);
+  }
   return (
     <div>
       <h1>{potluck?.name}</h1>
+      <Snackbar
+        open={showSnackbar}
+        onClose={() => setShowSnackbar(false)}
+        message="Link copied to clipboard"
+        autoHideDuration={3000}
+
+      />
+      <Button
+        variant="text"
+        color="primary"
+        size="small"
+        onClick={copyLink}
+      >
+        Copy Link
+      </Button>
       <div className="grid">
         <p>
           <Label text="Description" />
